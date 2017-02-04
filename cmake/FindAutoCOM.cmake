be2efc42fd3305e9792cf022bc31395dbca7ce0a
Alex Huszagh <ahuszagh@gmail.com>
2017-02-03 20:07:27 -0800

    Fixed naming issues with MSVC

--- cmake/FindAutoCOM.cmake
+++ cmake/FindAutoCOM.cmake
@@ -29,7 +29,7 @@ FIND_PATH(AutoCOM_INCLUDE_DIRS autocom.hpp)
 FIND_PATH(ITL_INCLUDE_DIR itl/memory.hpp)
 list(APPEND AutoCOM_INCLUDE_DIRS ${ITL_INCLUDE_DIR})
 FIND_PROGRAM(AutoCOM_EXECUTABLE
-    NAMES autocom
+    NAMES autocomconf
     PATH_SUFFIXES bin
     DOC "The autocom executable for the AutoCOM to use"
 )
